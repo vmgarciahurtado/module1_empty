a892d61da37b771d6f4e29a35915b7ced667e466
vamgarcia <victor.garcia@celuweb.com>
2022-05-26 08:53:59 -0500

    first commit

---
+++ prueba.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Module1Test extends StatelessWidget {
+  const Module1Test({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple,
+    );
+  }
+}
